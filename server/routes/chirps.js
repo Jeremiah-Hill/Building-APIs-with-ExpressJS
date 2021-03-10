02c37061048db7be1369bd9049903e3ada95c351
Jeremiah-Hill <77300280+Jeremiah-Hill@users.noreply.github.com>
2021-03-10 08:01:32 -0600

    Add files via upload

---
+++ server/routes/chirps.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { GetChirps } = require('../utils/chirpstore');
+const chirpstore = require('../utils/chirpstore');
+const ChirpStore = require('../utils/chirpstore')
+
+const router = express.Router();
+
+// GET http:Localhost:3000/api/chirps/123
+router.get('/:id/details', (req,res) => {
+    const id = req.params.id;
+    const chirp = ChirpStore.GetChirps(id)
+    res.json(chirps);
+});
+
+// GET http:Localhost:3000/api/chirps/
+router.get('/', (req, res) => {
+    res.json({ msg: 'testing get all chirps' });
+});
+
+// POST http:Localhost:3000/api/chirps/
+// { title: string, content: string }
+router.post('/', (req, res) => {
+    ChirpStore.CreateChirp(newChirp);
+    const newChirp = req.body;
+    res.json({ msg: 'new chirp added', test: newChirp });
+});
+
+// POST http:Localhost:3000/api/chirps/123
+// { title: string, content: string }
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const editedChirp = req.body;
+    ChirpStore.UpdateChirp(id, editedTodo);
+    res.json({ msg: `chirp id ${id} edited`, test: editedChirp});
+});
+
+// DELETE http:Localhost:3000/api/chirps/123
+router.delete('/:id/details', (req,res) => {
+    const id = req.params.id;
+    ChirpStore.DeleteChirp(id)
+    res.json({msg: `chirp id ${id} deleted`});
+});
+
+module.exports = router;
